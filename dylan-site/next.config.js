167738cd31b730b2ad05000368f51f4be0ba55f9
Dylan Borchert <dylanbor01@gmail.com>
2023-11-16 18:55:45 -0700

    fixed next image

--- dylan-site/next.config.js
+++ dylan-site/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		domains: ["localhost", "strapi.phantommedia.online"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
