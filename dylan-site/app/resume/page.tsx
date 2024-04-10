7bee690428785a2d0dd0ea963fd0d2f24597dbbd
Dylan Borchert <dylanbor01@gmail.com>
2024-04-09 21:09:34 -0600

    removed metadata

--- dylan-site/app/resume/page.tsx
+++ dylan-site/app/resume/page.tsx
@@ -1,15 +1,5 @@
-"use client"
 import { useEffect } from "react"
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-    title: 'dylanborchert.ca',
-    description: 'My resmue',
-    robots: 'index, follow',
-}
 export default function Resume() {
-
-
     useEffect(() => {
         setTimeout(() => {
             window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ?mute=0;autoplay=1"
