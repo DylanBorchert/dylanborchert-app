c4a065384c9671872d891201c8ee72c4ce7fe406
Dylan Borchert <dylanbor01@gmail.com>
2024-04-09 20:13:50 -0600

    hehe

--- dylan-site/app/resume/page.tsx
+++ dylan-site/app/resume/page.tsx
@@ -1,10 +1,14 @@
-import { use } from "react"
+import { useEffect } from "react"
 
 export default function Resume() {
 
-
+    useEffect(() => {
+        setTimeout(() => {
+            window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        }, 30)
+    }, [])
 
     return (
-        <div>page</div>
+        <div>hehe</div>
     )
 }
